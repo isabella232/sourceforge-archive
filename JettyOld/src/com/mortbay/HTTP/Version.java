a468ada13804346e8ef253a752faa25b3c5e1bce
gregw <>
1999-01-26 17:56:06 +0000

    Initial revision

---
+++ JettyOld/src/com/mortbay/HTTP/Version.java
@@ -0,0 +1,11 @@
+// ========================================================================
+// Copyright (c) 1999 Mort Bay Consulting (Australia) Pty. Ltd.
+// $Id$
+// ========================================================================
+
+package com.mortbay.HTTP;
+
+public class Version
+{
+    public static final String __jetty = "MortBay-Jetty-2.1.B1";
+};
