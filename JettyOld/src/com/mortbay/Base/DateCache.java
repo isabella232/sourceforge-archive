531ed5600eee6d978425f11cc2534394f8441058
gregw <>
1999-03-03 22:59:47 +0000

    Initial revision

---
+++ JettyOld/src/com/mortbay/Base/DateCache.java
@@ -0,0 +1,65 @@
+// ========================================================================
+// Copyright (c) Kent Johnson <KJohnson@transparent.com>
+// $Id$
+// ========================================================================
+
+package com.mortbay.Util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/* ------------------------------------------------------------ */
+/**  DateCache.
+ * Computes String representations of Dates and caches
+ * the results
+ * so that subsequent requests for the same Date will be fast. It compares
+ * Dates to the second, so don't use DateCache with date formats that show
+ * milliseconds.
+ *
+ * @see
+ * @version 1.0 Wed Mar  3 1999
+ * @author Kent Johnson <KJohnson@transparent.com>
+ */
+
+public final class DateCache
+{
+    private long lastSeconds = 0;
+    private String lastResult = null;
+    private SimpleDateFormat formatter;
+	
+    /** Make a DateCache that will use the given format */
+    public DateCache(SimpleDateFormat formatter)
+    {
+	this.formatter = formatter;
+    }
+
+    /* ------------------------------------------------------------ */
+    /** Constructor.
+     * Make a DateCache that will use a default format. The default format
+     * generates the same results as Date.toString().
+     */
+    public DateCache()
+    {
+	this(new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.US));
+	formatter.setTimeZone(TimeZone.getDefault());
+    }
+
+    /* ------------------------------------------------------------ */
+    /** Format a date according to our stored formatter.
+     * @param inDate 
+     * @return Formatted date
+     */
+    public synchronized String format(Date inDate)
+    {
+	long seconds = inDate.getTime() / 1000;
+	if (lastSeconds != seconds)
+	{
+	    lastSeconds = seconds;
+	    lastResult = formatter.format(inDate);
+	}
+		
+	return lastResult;
+    }
+}	
