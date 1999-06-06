0bd883d5c519fd433fab280a56a70fca3f9129ea
gregw <>
1999-06-06 23:54:06 +0000

    Initial revision

---
+++ JettyOld/src/com/mortbay/Servlets/Exit.java
@@ -0,0 +1,28 @@
+// ========================================================================
+// Copyright (c) 1999 Mort Bay Consulting (Australia) Pty. Ltd.
+// $Id$
+// ========================================================================
+
+package com.mortbay.Servlets;
+import com.mortbay.Base.*;
+import com.mortbay.HTML.*;
+import com.mortbay.HTTP.*;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/**
+ * This is an example of a simple Servlet
+ */
+public class Exit extends HttpServlet
+{
+    /* ------------------------------------------------------------ */
+    public void doGet(HttpServletRequest sreq, HttpServletResponse sres) 
+	throws ServletException, IOException
+    {
+	Code.warning("Exit requested");
+	System.exit(0);
+    }    
+}
