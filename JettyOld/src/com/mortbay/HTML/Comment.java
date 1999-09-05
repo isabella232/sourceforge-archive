9b56fb84112dc205e52c6e38bcbc4a564b752a10
gregw <>
1999-09-05 00:04:57 +0000

    Initial revision

---
+++ JettyOld/src/com/mortbay/HTML/Comment.java
@@ -0,0 +1,27 @@
+// ========================================================================
+// Copyright (c) 1999 Mort Bay Consulting (Australia) Pty. Ltd.
+// $Id$
+// ========================================================================
+
+package com.mortbay.HTML;
+import java.io.*;
+import java.util.*;
+
+import com.mortbay.Base.Code;
+
+/* ------------------------------------------------------------ */
+/** HTML Comment
+ * @version 1.0 Sat Sep  4 1999
+ * @author Greg Wilkins (gregw)
+ */
+public class Comment extends Composite
+{
+    /* ----------------------------------------------------------------- */
+    public void write(Writer out)
+	 throws IOException
+    {
+	out.write("<!--\n");
+        super.write(out);
+        out.write("\n-->");
+    }
+};
