8ee9916c13977c88fc49df6547a8081812f4157d
gregw <>
1999-10-14 11:24:12 +0000

    231

--- JettyOld/src/com/mortbay/HTTP/Version.java
+++ JettyOld/src/com/mortbay/HTTP/Version.java
@@ -7,5 +7,5 @@ package com.mortbay.HTTP;
 
 public class Version
 {
-    public static final String __jetty = "MortBay-Jetty-2.3.0";
+    public static final String __jetty = "MortBay-Jetty-2.3.1";
 };
