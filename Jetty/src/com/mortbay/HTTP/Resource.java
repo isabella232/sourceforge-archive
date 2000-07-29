c271f75c26f02563c886e1d15edc409ffd40b4ed
gregwilkins <>
2000-07-29 10:42:18 +0000

    Start of a resource handler

---
+++ Jetty/src/com/mortbay/HTTP/Resource.java
@@ -0,0 +1,429 @@
+package com.mortbay.HTTP;
+
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import com.mortbay.Util.*;
+import java.security.*;
+
+
+/* ------------------------------------------------------------ */
+/**
+ * Class that represents a resource accessible through the file or URL 
+ * <p>
+ *
+ * @version $Id$
+ * @author Nuno Preguiça 
+ * @author Greg Wilkins (gregw)
+ */
+public class Resource 
+{
+    private URL _url;
+    private URLConnection _connection;
+    private InputStream _in=null;
+
+    /* ------------------------------------------------------------ */
+    /** 
+     * @param url 
+     * @return 
+     */
+    public static Resource newResource(URL url)
+	throws IOException
+    {
+	if (url==null)
+	    return null;
+	
+	URLConnection connection=url.openConnection();
+	Permission perm = connection.getPermission();
+	if (perm instanceof java.io.FilePermission)
+	{
+	    File file =new File(perm.getName());
+	    if (file.isDirectory() && !url.getFile().endsWith("/"))
+		url=new URL(url.toString()+"/");
+	    return new FileResource(url,connection,file);
+	}
+	return new Resource(url,connection);
+    }
+
+    /* ------------------------------------------------------------ */
+    /** Construct a resource from a string.
+     * If the string is not a URL, it is treated as an absolute or
+     * relative file path.
+     * @param resource. 
+     * @return 
+     */
+    public static Resource newResource(String resource)
+	throws MalformedURLException, IOException
+    {
+	URL url=null;
+	try
+	{
+	    // Does this format as a URL?
+	    url = new URL(resource);
+	}
+	catch(MalformedURLException e)
+	{
+	    url = new File(resource).toURL();
+	}
+	
+	return newResource(url);
+    }
+
+    
+    /* ------------------------------------------------------------ */
+    private Resource(URL url, URLConnection connection)
+    {
+	_url = url;
+	_connection=connection;
+    }
+
+    /* ------------------------------------------------------------ */
+    private boolean checkConnection()
+    {
+	if (_connection==null)
+	{
+	    try{
+		_connection=_url.openConnection();
+	    }
+	    catch(IOException e)
+	    {
+		Code.ignore(e);
+	    }
+	}
+	return _connection!=null;
+    }
+
+    /* ------------------------------------------------------------ */
+    protected void finalize()
+    {
+	release();
+    }
+    
+    /* ------------------------------------------------------------ */
+    /** Release any resources held by the resource 
+     */
+    public void release()
+    {
+	if (_in!=null)
+	{
+	    try{_in.close();}catch(IOException e){Code.ignore(e);}
+	    _in=null;
+	}
+	
+	if (_connection!=null)
+	{
+	    _connection=null;
+	}
+    }
+    
+    /* ------------------------------------------------------------ */
+    /**
+     * Returns true if the respresenetd resource exists.
+     */
+    public boolean exists()
+    {
+	try
+	{
+	    if (checkConnection())
+		_in = _connection.getInputStream();
+	}
+	catch (IOException e)
+	{
+	    Code.ignore(e);
+	}
+	return _in!=null;
+    }
+
+    /* ------------------------------------------------------------ */
+    /**
+     * Returns true if the respresenetd resource is a container/directory.
+     * If the resource is not a file, resources ending with "/" are
+     * considered directories.
+     */
+    public boolean isDirectory()
+    {
+	return _url.getFile().endsWith("/");
+    }
+
+    
+    /* ------------------------------------------------------------ */
+    /**
+     * Returns the last modified time
+     */
+    public long lastModified()
+    {
+	if (checkConnection())
+	    return _connection.getLastModified();
+	return -1;
+    }
+
+
+    /* ------------------------------------------------------------ */
+    /**
+     * Return the length of the resource
+     */
+    public long length()
+    {
+	if (checkConnection())  
+	    return _connection.getContentLength();
+	return -1;
+    }
+
+    /* ------------------------------------------------------------ */
+    /**
+     * Returns an URL representing the given resource
+     */
+    public URL getURL()
+    {
+	return _url;
+    }
+
+    /* ------------------------------------------------------------ */
+    /**
+     * Returns an File representing the given resource or NULL if this
+     * is not possible.
+     */
+    public File getFile()
+    {
+	return null;
+    }
+    
+    /* ------------------------------------------------------------ */
+    /**
+     * Returns the name of the resource
+     */
+    public String getName()
+    {
+	return _url.getFile();
+    }
+	
+    /* ------------------------------------------------------------ */
+    /**
+     * Returns an input stream to the resource
+     */
+    public synchronized java.io.InputStream getInputStream()
+	throws java.io.IOException
+    {
+	if (checkConnection())  
+	    throw new IOException( "Invalid resource");
+
+	if( _in != null)
+	{
+	    InputStream in = _in;
+	    _in=null;
+	    return _in;
+	}
+
+	return _connection.getInputStream();
+    }
+
+    
+    /* ------------------------------------------------------------ */
+    /**
+     * Returns an output stream to the resource
+     */
+    public OutputStream getOutputStream()
+	throws java.io.IOException, SecurityException
+    {
+	throw new IOException( "Output not supported");
+    }
+
+    /* ------------------------------------------------------------ */
+    /**
+     * Deletes the given resource
+     */
+    public boolean delete()
+	throws SecurityException
+    {
+	throw new SecurityException( "Delete not supported");
+    }
+
+    /* ------------------------------------------------------------ */
+    /**
+     * Rename the given resource
+     */
+    public boolean renameTo( Resource dest)
+	throws SecurityException
+    {
+	throw new SecurityException( "RenameTo not supported");
+    }
+
+    
+    /* ------------------------------------------------------------ */
+    /**
+     * Returns a list of resource names contained in the given resource
+     */
+    public synchronized String[] list()
+    {
+	return null;
+    }
+    
+    /* ------------------------------------------------------------ */
+    /**
+     * Returns the resource contained inside the current resource with the
+     * given name
+     */
+    public Resource relative(String name)
+	throws IOException,MalformedURLException
+    {
+	return newResource(new URL(_url,name));
+    }
+
+    /* ------------------------------------------------------------ */
+    /** 
+     * @return 
+     */
+    public String toString()
+    {
+	return _url.toString();
+    }
+    
+	
+    /* ------------------------------------------------------------ */
+    /** 
+     * @param o
+     * @return 
+     */
+    public boolean equals( Object o)
+    {
+	return o instanceof Resource &&
+	    _url.equals(((Resource)o)._url);
+    }
+
+    /* ------------------------------------------------------------ */
+    /* ------------------------------------------------------------ */
+    private static class FileResource extends Resource
+    {
+	File _file;
+	
+	/* -------------------------------------------------------- */
+	private FileResource(URL url, URLConnection connection, File file)
+	{
+	    super(url,connection);
+	    _file=file;
+	}
+
+
+	/* -------------------------------------------------------- */
+	/**
+	 * Returns true if the respresenetd resource exists.
+	 */
+	public boolean exists()
+	{
+	    return _file.exists();
+	}
+	
+	/* -------------------------------------------------------- */
+	/**
+	 * Returns the last modified time
+	 */
+	public long lastModified()
+	{
+	    return _file.lastModified();
+	}
+
+	/* -------------------------------------------------------- */
+	/**
+	 * Returns true if the respresenetd resource is a container/directory.
+	 */
+	public boolean isDirectory()
+	{
+	    return _file.isDirectory();
+	}
+
+	/* --------------------------------------------------------- */
+	/**
+	 * Return the length of the resource
+	 */
+	public long length()
+	{
+	    return _file.length();
+	}
+	
+
+	/* --------------------------------------------------------- */
+	/**
+	 * Returns the name of the resource
+	 */
+	public String getName()
+	{
+	    return _file.getName();
+	}
+	
+	/* ------------------------------------------------------------ */
+	/**
+	 * Returns an File representing the given resource or NULL if this
+	 * is not possible.
+	 */
+	public File getFile()
+	{
+	    return _file;
+	}
+	
+	/* --------------------------------------------------------- */
+	/**
+	 * Returns an input stream to the resource
+	 */
+	public InputStream getInputStream() throws IOException
+	{
+	    return new FileInputStream(_file);
+	}
+	
+	/* --------------------------------------------------------- */
+	/**
+	 * Returns an output stream to the resource
+	 */
+	public OutputStream getOutputStream()
+	    throws java.io.IOException, SecurityException
+	{
+	    return new FileOutputStream(_file);
+	}
+	
+	/* --------------------------------------------------------- */
+	/**
+	 * Deletes the given resource
+	 */
+	public boolean delete()
+	    throws SecurityException
+	{
+	    return _file.delete();
+	}
+
+	/* --------------------------------------------------------- */
+	/**
+	 * Rename the given resource
+	 */
+	public boolean renameTo( Resource dest)
+	    throws SecurityException
+	{
+	    if( dest instanceof FileResource)
+		return _file.renameTo( ((FileResource)dest)._file);
+	    else
+		return false;
+	}
+
+	/* --------------------------------------------------------- */
+	/**
+	 * Returns a list of resources contained in the given resource
+	 */
+	public String[] list()
+	{
+	    return _file.list();
+	}
+	
+	/* ------------------------------------------------------------ */
+	/** 
+	 * @param o
+	 * @return 
+	 */
+ 	public boolean equals( Object o)
+	{
+	    return o instanceof FileResource &&
+		_file.equals(((FileResource)o)._file);
+	}
+    }
+}
+
+
+
+
--- Jetty/src/com/mortbay/HTTP/TestHarness.java
+++ Jetty/src/com/mortbay/HTTP/TestHarness.java
@@ -176,7 +176,6 @@ public class TestHarness
         }
     }
     
-    
     /* --------------------------------------------------------------- */
     public static void httpFields()
         throws IOException
@@ -368,15 +367,61 @@ public class TestHarness
         }
     }
     
+    
+    /* --------------------------------------------------------------- */
+    public static void resourceTest()
+    {
+        Test t = new Test("com.mortbay.HTTP.Resource");
+        try
+        {
+	    Resource r =
+		Resource.newResource("file:"+
+				     System.getProperty("user.dir")+
+				     "/TestHarness.java");
+	    t.check(r.exists(),"File URL exists");
+	    t.check(!r.isDirectory(),"File URL not directory");
+	    r = Resource.newResource(System.getProperty("user.dir")+
+				     "/TestHarness.java");
+	    t.check(r.exists(),"Abs File exists");
+	    t.check(!r.isDirectory(),"Abs URL not directory");
+	    r = Resource.newResource("TestHarness.java");
+	    t.check(r.exists(),"Rel File exists");
+	    t.check(!r.isDirectory(),"Rel URL not directory");
+	    
+	    r = Resource.newResource("file:"+
+				     System.getProperty("user.dir")+
+				     "/TestHarness.java");
+	    r = r.relative("Resource.java");
+	    t.check(r.exists(),"Relative resource exists");
+	    r = r.relative("UnknownFile");
+	    t.check(!r.exists(),"Relative resource ! exists");
+	    
+
+	    r = Resource.newResource("file:"+System.getProperty("user.dir"));
+	    t.check(r.exists(),"Dir URL exists");
+	    t.check(r.isDirectory(),"Dir URL directory");
+	    r = r.relative("Resource.java");
+	    t.check(r.exists(),"Relative resource exists");
+	    r = r.relative("UnknownFile");
+	    t.check(!r.exists(),"Relative resource ! exists");
+	}
+        catch(Exception e)
+        {
+            Code.warning(e);
+            t.check(false,e.toString());
+        }
+    }
+    
     /* ------------------------------------------------------------ */
     public static void main(String[] args)
     {
         try{
-            chunkInTest();
+	    chunkInTest();
             chunkOutTest();
             filters();
             httpFields();
             pathMap();
+	    resourceTest();
 
             TestRequest.test();
             TestRFC2616.test();
