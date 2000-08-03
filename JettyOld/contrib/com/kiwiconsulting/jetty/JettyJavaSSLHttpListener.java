1dfb9642d29dd311617eacc04e333685739eff1d
pbannister <>
2000-08-03 23:20:11 +0000

    Note JSSE version dependency.  Fix bad line wrap.

--- JettyOld/contrib/com/kiwiconsulting/jetty/JettyJavaSSLHttpListener.java
+++ JettyOld/contrib/com/kiwiconsulting/jetty/JettyJavaSSLHttpListener.java
@@ -1,4 +1,3 @@
-
 package com.kiwiconsulting.jetty;
 
 import java.io.IOException;
@@ -34,17 +33,16 @@ import com.mortbay.HTTP.HttpServer;
  *
  *<p>
  *
- * This implementation is trash.  I just wanted to get something
-working.
+ * This implementation is trash.  I just wanted to get something working.
  *
  * @author Court Demas (court@kiwiconsulting.com)
  **/
+
 public class JettyJavaSSLHttpListener extends HttpListener
 {
     // location of the keystore (defaults to ~/.keystore)
     public static final String KEYSTORE_PROPERTY = "jetty.ssl.keystore";
-    public static final String DEFAULT_KEYSTORE  =
-	System.getProperty("user.home" ) + File.separator + ".keystore";
+    public static final String DEFAULT_KEYSTORE  = System.getProperty( "user.home" ) + File.separator + ".keystore";
 
     // password for the keystore
     public static final String PASSWORD_PROPERTY = "jetty.ssl.password";
@@ -57,91 +55,83 @@ public class JettyJavaSSLHttpListener extends HttpListener
     }
 
     /* ------------------------------------------------------------ */
-    /** Constructor. 
-     * @param p_address 
-     * @param p_server 
-     * @exception IOException 
+    /** Constructor.
+     * @param p_address
+     * @param p_server
+     * @exception IOException
      */
-    public JettyJavaSSLHttpListener(InetAddrPort p_address,
-				    HttpServer p_server )
+    public JettyJavaSSLHttpListener( InetAddrPort p_address, HttpServer p_server )
         throws IOException
     {
-	this( p_address, p_server, 0, 0, 0 );
+        this( p_address, p_server, 0, 0, 0 );
     }
 
     /* ------------------------------------------------------------ */
-    /** Constructor. 
-     * @param p_address 
-     * @param p_server 
-     * @param p_minThreads 
-     * @param p_maxThreads 
-     * @param p_maxIdleTimeMs 
-     * @exception IOException 
+    /** Constructor.
+     * @param p_address
+     * @param p_server
+     * @param p_minThreads
+     * @param p_maxThreads
+     * @param p_maxIdleTimeMs
+     * @exception IOException
      */
     public JettyJavaSSLHttpListener( InetAddrPort p_address,
-				     HttpServer p_server,
+                                     HttpServer p_server,
                                      int p_minThreads,
-				     int p_maxThreads,
-				     int p_maxIdleTimeMs)
+                                     int p_maxThreads,
+                                     int p_maxIdleTimeMs )
         throws IOException
     {
-	super( p_address, p_server, p_minThreads, p_maxThreads,
-	       p_maxIdleTimeMs );
-	if( p_address.getPort() == 0 )
-	{
-	    p_address.setPort( 80 );
-	    super.setAddress( p_address.getInetAddress(), p_address.getPort() );
-	}
+        super( p_address, p_server, p_minThreads, p_maxThreads, p_maxIdleTimeMs );
+        if( p_address.getPort() == 0 )
+        {
+            p_address.setPort( 80 );
+            super.setAddress( p_address.getInetAddress(), p_address.getPort() );
+        }
 
-	System.err.println( "JettyJavaSSLHttpListener started on " +
-			    p_address );
+        System.err.println( "JettyJavaSSLHttpListener started on " + p_address );
     }
 
     /* ------------------------------------------------------------ */
-    /* 
-     * @return 
-     * @exception Exception 
+    /*
+     * @return
+     * @exception Exception
      */
     private SSLServerSocketFactory createFactoryHACK()
         throws Exception
     {
+        String password = System.getProperty( PASSWORD_PROPERTY, DEFAULT_PASSWORD );
+        Log.event(PASSWORD_PROPERTY+"="+password);
+        char[] pwChars = password.toCharArray();
 
-        String password = System.getProperty( PASSWORD_PROPERTY,
-					      DEFAULT_PASSWORD);
-	Log.event(PASSWORD_PROPERTY+"="+password);
-	char[] pwChars = password.toCharArray();
-	
-        KeyStore ks = KeyStore.getInstance( "JKS" ); 
-            
-        String keystore = System.getProperty( KEYSTORE_PROPERTY,
-					      DEFAULT_KEYSTORE );
-	Log.event(KEYSTORE_PROPERTY+"="+keystore);
-	
+        KeyStore ks = KeyStore.getInstance( "JKS" );
+
+        String keystore = System.getProperty( KEYSTORE_PROPERTY, DEFAULT_KEYSTORE );
+        Log.event(KEYSTORE_PROPERTY+"="+keystore);
+
         ks.load( new FileInputStream( new File( keystore ) ), pwChars );
 
-        KeyManagerFactory km = KeyManagerFactory.getInstance( "SunX509"); 
+        KeyManagerFactory km = KeyManagerFactory.getInstance( "SunX509" );
         km.init( ks, pwChars );
-        KeyManager[] kma = km.getKeyManagers();                        
-           
-        TrustManagerFactory tm = TrustManagerFactory.getInstance("SunX509" );
-        tm.init( ks ); 
-        TrustManager[] tma = tm.getTrustManagers(); 
-        SSLContext sslc = SSLContext.getInstance( "SSL" ); 
-        sslc.init( kma, tma, SecureRandom.getInstance( "SHA1PRNG" ) ); 
+        KeyManager[] kma = km.getKeyManagers();
+
+        TrustManagerFactory tm = TrustManagerFactory.getInstance( "SunX509" );
+        tm.init( ks );
+        TrustManager[] tma = tm.getTrustManagers();
+        SSLContext sslc = SSLContext.getInstance( "SSL" );
+        sslc.init( kma, tma, SecureRandom.getInstance( "SHA1PRNG" ) );
         SSLServerSocketFactory ssfc = sslc.getServerSocketFactory();
         return ssfc;
     }
-    
 
     /* ------------------------------------------------------------ */
-    /** 
-     * @param p_address 
-     * @param p_acceptQueueSize 
-     * @return 
-     * @exception IOException 
+    /**
+     * @param p_address
+     * @param p_acceptQueueSize
+     * @return
+     * @exception IOException
      */
-    protected ServerSocket newServerSocket( InetAddrPort p_address,
-					    int p_acceptQueueSize )
+    protected ServerSocket newServerSocket( InetAddrPort p_address, int p_acceptQueueSize )
         throws IOException
     {
         SSLServerSocketFactory factory = null;
@@ -149,24 +139,19 @@ public class JettyJavaSSLHttpListener extends HttpListener
 
         try
         {
-	    //factory = createFactory();
+            //factory = createFactory();
             factory = createFactoryHACK();
 
-	    // yeah right...
-	    //factory = (SSLServerSocketFactory)
-	    SSLServerSocketFactory.getDefault();
+            // yeah right...
+            //factory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
 
             if( p_address == null )
             {
-                socket = (SSLServerSocket)
-		    factory.createServerSocket(0, p_acceptQueueSize );
+                socket = (SSLServerSocket) factory.createServerSocket( 0, p_acceptQueueSize );
             }
             else
             {
-                socket = (SSLServerSocket)
-		    factory.createServerSocket(p_address.getPort(),
-					       p_acceptQueueSize,
-					       p_address.getInetAddress() );
+                socket = (SSLServerSocket) factory.createServerSocket( p_address.getPort(), p_acceptQueueSize, p_address.getInetAddress() );
             }
         }
         catch( Exception e )
@@ -181,27 +166,24 @@ public class JettyJavaSSLHttpListener extends HttpListener
     }
 
     /* ------------------------------------------------------------ */
-    /** 
-     * @param p_serverSocket 
-     * @return 
-     * @exception IOException 
+    /**
+     * @param p_serverSocket
+     * @return
+     * @exception IOException
      */
     protected Socket accept( ServerSocket p_serverSocket )
         throws IOException
     {
-        
-	try
-	{
+        try
+        {
             SSLSocket s = (SSLSocket) p_serverSocket.accept();
-	    s.startHandshake();  // this should block until the
-	    // handshaking is done
+            s.startHandshake();  // this should block until the handshaking is done
             return (Socket) s;
-	}
-	catch( SSLException e )
-	{
+        }
+        catch( SSLException e )
+        {
             e.printStackTrace();
-	    throw new IOException( e.getMessage() );
-	}
+            throw new IOException( e.getMessage() );
+        }
     }
 }
-
