72af5e16c6f307101323c1bd460db36683aa4fb7
juanco <>
2000-10-13 23:41:24 +0000

    Made the class loader (almost) Java2 compliant

--- Jetty/src/com/mortbay/HTTP/Handler/Servlet/ServletLoader.java
+++ Jetty/src/com/mortbay/HTTP/Handler/Servlet/ServletLoader.java
@@ -18,7 +18,7 @@ import java.util.HashMap;
 
 /* ------------------------------------------------------------ */
 /** Servlet Class Loader.
- * 
+ *
  * <h4>Notes</h4>
  * The load search order is:<NL>
  * <LI>Loader cache.
@@ -32,11 +32,10 @@ import java.util.HashMap;
  */
 public class ServletLoader extends ClassLoader
 {
-    private HashMap _cache = new HashMap(37);
     private ResourcePath _classPath;
-    
+
     /* ------------------------------------------------------------ */
-    /** Constructor. 
+    /** Constructor.
      * @param servletClassPath Coma separated path of filenames or URLs
      * pointing to directories or jar files. Directories should end
      * with '/'.
@@ -46,9 +45,9 @@ public class ServletLoader extends ClassLoader
     {
         this(null,false);
     }
-    
+
     /* ------------------------------------------------------------ */
-    /** Constructor. 
+    /** Constructor.
      * @param servletClassPath Coma separated path of filenames or URLs
      * pointing to directories or jar files. Directories should end
      * with '/'.
@@ -58,64 +57,54 @@ public class ServletLoader extends ClassLoader
     {
         this(servletClassPath,false);
     }
-    
-    
+
+
     /* ------------------------------------------------------------ */
-    /** Constructor. 
+    /** Constructor.
      * @param servletClassPath Coma separated path of filenames or URLs
      * pointing to directories or jar files. Directories should end
      * with '/'.
      * @param quiet If true, non existant paths are not reported
-     * @exception IOException 
+     * @exception IOException
      */
     public ServletLoader(String servletClassPath,
                          boolean quiet)
         throws IOException
     {
+        super(ServletLoader.class.getClassLoader());
         Code.debug("new ServletLoader(",servletClassPath,")");
         if (servletClassPath!=null && servletClassPath.length()>0)
             _classPath=new ResourcePath(servletClassPath,quiet);
     }
 
-    
+
     /* ------------------------------------------------------------ */
-    /** Load a class.
-     * Classes from the "javax.servlet." packages are loaded from the
-     * builtin ZipResource. Other classes are loaded from a call to
+    /** Find and define a class.
+     * Classes from the "javax.servlet." and  "com.mortbay." packages are loaded
+     * only from the system class loader. Other classes are loaded using
      * loadServletClass.
      * @param name Class name (without ".class");
      * @return The Class instance
-     * @exception ClassNotFoundException 
+     * @exception ClassNotFoundException
      */
-    final public Class loadClass(String name)
+    protected Class findClass(String name)
         throws ClassNotFoundException
     {
         try
         {
-            // look in the cache
-            Class c=(Class)_cache.get(name);
-            if (c!=null)
-                return c;
-        
-            // If it is a system class, it must be loaded from the
-            // base loader
-            if (isSystemClass(name))
-                return findSystemClass(name);    
-            
             // try loading from the handler class path
-            c=loadFromClassPath(name);
-            if (c!=null)
+            byte data[] =loadFromClassPath(name);
+            if (data != null)
             {
-                if (Code.verbose())Code.debug("loaded  ",name);
-                _cache.put(name,c);
+                Class c = defineClass(name,data,0,data.length);
+                if (Code.verbose())Code.debug("Loaded ",c," for ",name);
                 return c;
             }
-
-            return findSystemClass(name);
+            return super.findClass(name);
         }
         catch(ClassNotFoundException e)
         {
-            throw e;
+          throw e;
         }
         catch(Throwable e)
         {
@@ -125,39 +114,69 @@ public class ServletLoader extends ClassLoader
     }
 
     /* ------------------------------------------------------------ */
+    /** Load a class.
+     * This method is not compliant with the Java2 spec. It is here only
+     * to avoid servlets being loaded from the system classpath. To obtain
+     * compliance, simply remove this method.
+     * @param name Class name (without ".class");
+     * @param resolve True if the class should be resolved when loaded.
+     * @return The Class instance
+     * @exception ClassNotFoundException
+     */
+    protected synchronized Class loadClass(String name, boolean resolve)
+        throws ClassNotFoundException
+    {
+    // First, check if the class has already been loaded
+    Class c = findLoadedClass(name);
+    if (c == null)
+    {
+        try
+        {
+            c = findClass(name);
+        }
+        catch (ClassNotFoundException e)
+        {
+            if (getParent() != null)
+                c = getParent().loadClass(name);
+            else
+                c = findSystemClass(name);
+        }
+    }
+    if (resolve) {
+        resolveClass(c);
+    }
+    return c;
+    }
+
+
+    /* ------------------------------------------------------------ */
     /** Load a class from the servlet handler classpath.
-     * This method is used by by loadClass to load classes
+     * This method is used by by findClass to load classes
      * from the servlet class path and should be specialized by
      * derived ServletLoaders.
      * @param name Class name (without ".class");
-     * @return the class or null if not found.
+     * @return the A byte array with the class data.
      */
-    protected Class loadFromClassPath(String name)
+    protected byte[] loadFromClassPath(String name)
     {
         if (_classPath==null)
             return null;
-        
-        Class c;        
+
         //remove the .class
         if (name.endsWith(".class"))
             name = name.substring(0,name.lastIndexOf('.'));
-        
-        // Look for a cached class
-        c=(Class)_cache.get(name);
-        if (c!=null)
-            return c;
 
         // get the package name
         String packageName="";
         if (name.indexOf(".")>=0)
             packageName=name.substring(0,name.lastIndexOf('.'));
-        
+
         // Check permission
         SecurityManager sm=System.getSecurityManager();
         if (sm!=null)
             sm.checkPackageAccess(packageName);
-        
-        // Load & define
+
+        // Load the class data
         String filename = name.replace('.',File.separatorChar)+".class";
         InputStream in=_classPath.getInputStream(filename);
         if (in!=null)
@@ -166,10 +185,8 @@ public class ServletLoader extends ClassLoader
                 ByteArrayOutputStream out =
                     new ByteArrayOutputStream(8192);
                 com.mortbay.Util.IO.copy(in,out);
-                
-                byte data[] = out.toByteArray();
-                c= defineClass(name,data,0,data.length);
-                _cache.put(name,c);
+                if (Code.verbose())Code.debug("Loaded ",name);
+                return out.toByteArray();
             }
             catch(Exception e)
             {
@@ -182,51 +199,30 @@ public class ServletLoader extends ClassLoader
             }
         }
 
-        if (Code.verbose())Code.debug("Loaded ",c," for ",name);
-        return c;
+        return null;
     }
-    
-    /* ------------------------------------------------------------ */
-    /** Get a resource as a stream.
-     * Return the raw input stream from a file in the classpath
-     * @param filename The filename of the resource
-     * @return An InputStream to the resource or null
-     */
-    final public InputStream getResourceAsStream(String filename)
-    {
-        if (Code.verbose()) Code.debug("Load resource as stream ",filename);
 
-        InputStream in=null;
-        if (_classPath!=null)
-            in=_classPath.getInputStream(filename);
-        if (in==null)
-            in=ClassLoader.getSystemResourceAsStream(filename);
-        
-        return in;
-    }
 
     /* ------------------------------------------------------------ */
     /** Get a resource.
      * Not implemented.
-     * @param filename 
-     * @return 
+     * @param filename
+     * @return
      */
-    public URL getResource(String filename)
+    protected URL findResource(String filename)
     {
-        Code.debug("Load resource ",filename);
-        URL url=null;
-        if (_classPath!=null)
-        {
-            Resource resource=_classPath.getResource(filename);
-            if (resource!=null)
-                url=resource.getURL();
-        }
-        if (url==null)
-            url=ClassLoader.getSystemResource(filename);
-        return url;
+    Code.debug("Load resource ",filename);
+    URL url=null;
+    if (_classPath!=null)
+    {
+        Resource resource=_classPath.getResource(filename);
+        if (resource!=null)
+        url=resource.getURL();
     }
-    
-    
+    return url;
+    }
+
+
     /* ------------------------------------------------------------ */
     /** Return true a class is modified.
      * Calls isServletModified().
@@ -234,59 +230,8 @@ public class ServletLoader extends ClassLoader
      * modified since their load time.
      */
     public boolean isModified()
-    {	
+    {
         return _classPath.isModified();
-    }
-    
-    
-    /* ------------------------------------------------------------ */
-    /** Load a class.
-     * @param name Class name (without ".class");
-     * @param resolve True if the class should be resolved when loaded. 
-     * @return The Class instance
-     * @exception ClassNotFoundException 
-     */
-    public Class loadClass(String name, boolean resolve)
-        throws ClassNotFoundException
-    {
-        Class c = loadClass(name);
-        
-        // resolve
-        if (resolve)
-            resolveClass(c);
-        return c;
-    }
-
-    
-    
-    /* ------------------------------------------------------------ */
-    /** Classes from packages starting with these strings can only
-     * be loaded from the builtin loader.
-     */
-    private final static String[] __systemClasses =
-    {
-        "java.",
-        "javax.",
-        "com.mortbay.Util.",
-        "com.mortbay.HTTP.",
-        "com.mortbay.HTML.",
-        "com.mortbay.FTP.",
-    };
-      
-
-    /* ------------------------------------------------------------ */
-    /* Is the class a system class.
-     * Check if the class is from java, javax or the com.mortbay
-     * packages.
-     * @param name The name of the class or package.
-     * @return true if the class name is in a system package.
-     */
-    private static boolean isSystemClass(String name)
-    {
-        for (int i=__systemClasses.length;i-->0;)
-            if (name.startsWith(__systemClasses[i]))
-                return true;
-        return false;
     }
 
 }
