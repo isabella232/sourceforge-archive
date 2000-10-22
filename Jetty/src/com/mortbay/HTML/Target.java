7a05efb1b0fddadeeec9991544be448ba3f49ed9
gregwilkins <>
2000-10-22 13:26:42 +0000

    Link target

---
+++ Jetty/src/com/mortbay/HTML/Target.java
@@ -0,0 +1,37 @@
+// ===========================================================================
+// Copyright (c) 1996 Mort Bay Consulting Pty. Ltd. All rights reserved.
+// $Id$
+// ---------------------------------------------------------------------------
+
+package com.mortbay.HTML;
+
+
+/* -------------------------------------------------------------------- */
+/** HTML Link Block
+ * This is a HTML reference (not a CSS Link).
+ * @see StyleLink
+ */
+public class Target extends Block
+{
+
+    /* ----------------------------------------------------------------- */
+    /** Construct Link
+     * @param target The target name 
+     */
+    public Target(String target)
+    {
+        super("A");
+        attribute("NAME",target);
+    }
+
+    /* ----------------------------------------------------------------- */
+    /** Construct Link
+     * @param target The target name 
+     * @param link Link Element
+     */
+    public Target(String target,Object link)
+    {
+        this(target);
+        add(link);
+    }
+}
