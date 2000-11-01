2ae5b188099a62795d0d86e62123bab382e6bcdf
gregwilkins <>
2000-11-01 02:11:37 +0000

    + Handle mime suffixes containing dots. + Added gz tgz tar.gz .z mime mappings. + Fixed default mimemap initialization bug

--- Jetty/src/com/mortbay/HTTP/HandlerContext.java
+++ Jetty/src/com/mortbay/HTTP/HandlerContext.java
@@ -340,8 +340,150 @@ public class HandlerContext implements LifeCycle
     }
     
     /* ------------------------------------------------------------ */
-    public Map getMimeMap()
+    public synchronized Map getMimeMap()
     {
+        if (_mimeMap==null)
+        {
+            _mimeMap = new HashMap();
+            _mimeMap.put("default","application/octet-stream");
+            _mimeMap.put("ai","application/postscript");
+            _mimeMap.put("aif","audio/x-aiff");
+            _mimeMap.put("aifc","audio/x-aiff");
+            _mimeMap.put("aiff","audio/x-aiff");
+            _mimeMap.put("asc","text/plain");
+            _mimeMap.put("au","audio/basic");
+            _mimeMap.put("avi","video/x-msvideo");
+            _mimeMap.put("bcpio","application/x-bcpio");
+            _mimeMap.put("bin","application/octet-stream");
+            _mimeMap.put("cdf","application/x-netcdf");
+            _mimeMap.put("class","application/java-vm");
+            _mimeMap.put("cpio","application/x-cpio");
+            _mimeMap.put("cpt","application/mac-compactpro");
+            _mimeMap.put("csh","application/x-csh");
+            _mimeMap.put("css","text/css");
+            _mimeMap.put("dcr","application/x-director");
+            _mimeMap.put("dir","application/x-director");
+            _mimeMap.put("dms","application/octet-stream");
+            _mimeMap.put("doc","application/msword");
+            _mimeMap.put("dvi","application/x-dvi");
+            _mimeMap.put("dxr","application/x-director");
+            _mimeMap.put("eps","application/postscript");
+            _mimeMap.put("etx","text/x-setext");
+            _mimeMap.put("exe","application/octet-stream");
+            _mimeMap.put("ez","application/andrew-inset");
+            _mimeMap.put("gif","image/gif");
+            _mimeMap.put("gtar","application/x-gtar");
+            _mimeMap.put("gz","application/gzip");
+            _mimeMap.put("gzip","application/gzip");
+            _mimeMap.put("hdf","application/x-hdf");
+            _mimeMap.put("hqx","application/mac-binhex40");
+            _mimeMap.put("html","text/html");
+            _mimeMap.put("htm","text/html");
+            _mimeMap.put("ice","x-conference/x-cooltalk");
+            _mimeMap.put("ief","image/ief");
+            _mimeMap.put("iges","model/iges");
+            _mimeMap.put("igs","model/iges");
+            _mimeMap.put("java","text/plain");
+            _mimeMap.put("jpeg","image/jpeg");
+            _mimeMap.put("jpe","image/jpeg");
+            _mimeMap.put("jpg","image/jpeg");
+            _mimeMap.put("js","application/x-javascript");
+            _mimeMap.put("jsp","text/plain");
+            _mimeMap.put("kar","audio/midi");
+            _mimeMap.put("latex","application/x-latex");
+            _mimeMap.put("lha","application/octet-stream");
+            _mimeMap.put("lzh","application/octet-stream");
+            _mimeMap.put("man","application/x-troff-man");
+            _mimeMap.put("me","application/x-troff-me");
+            _mimeMap.put("mesh","model/mesh");
+            _mimeMap.put("mid","audio/midi");
+            _mimeMap.put("midi","audio/midi");
+            _mimeMap.put("mif","application/vnd.mif");
+            _mimeMap.put("movie","video/x-sgi-movie");
+            _mimeMap.put("mov","video/quicktime");
+            _mimeMap.put("mp2","audio/mpeg");
+            _mimeMap.put("mp3","audio/mpeg");
+            _mimeMap.put("mpeg","video/mpeg");
+            _mimeMap.put("mpe","video/mpeg");
+            _mimeMap.put("mpga","audio/mpeg");
+            _mimeMap.put("mpg","video/mpeg");
+            _mimeMap.put("ms","application/x-troff-ms");
+            _mimeMap.put("msh","model/mesh");
+            _mimeMap.put("nc","application/x-netcdf");
+            _mimeMap.put("oda","application/oda");
+            _mimeMap.put("pbm","image/x-portable-bitmap");
+            _mimeMap.put("pdb","chemical/x-pdb");
+            _mimeMap.put("pdf","application/pdf");
+            _mimeMap.put("pgm","image/x-portable-graymap");
+            _mimeMap.put("pgn","application/x-chess-pgn");
+            _mimeMap.put("png","image/png");
+            _mimeMap.put("pnm","image/x-portable-anymap");
+            _mimeMap.put("ppm","image/x-portable-pixmap");
+            _mimeMap.put("ppt","application/vnd.ms-powerpoint");
+            _mimeMap.put("ps","application/postscript");
+            _mimeMap.put("qt","video/quicktime");
+            _mimeMap.put("ra","audio/x-pn-realaudio");
+            _mimeMap.put("ra","audio/x-realaudio");
+            _mimeMap.put("ram","audio/x-pn-realaudio");
+            _mimeMap.put("ras","image/x-cmu-raster");
+            _mimeMap.put("rgb","image/x-rgb");
+            _mimeMap.put("rm","audio/x-pn-realaudio");
+            _mimeMap.put("roff","application/x-troff");
+            _mimeMap.put("rpm","application/x-rpm");
+            _mimeMap.put("rpm","audio/x-pn-realaudio");
+            _mimeMap.put("rtf","application/rtf");
+            _mimeMap.put("rtf","text/rtf");
+            _mimeMap.put("rtx","text/richtext");
+            _mimeMap.put("sgml","text/sgml");
+            _mimeMap.put("sgm","text/sgml");
+            _mimeMap.put("sh","application/x-sh");
+            _mimeMap.put("shar","application/x-shar");
+            _mimeMap.put("silo","model/mesh");
+            _mimeMap.put("sit","application/x-stuffit");
+            _mimeMap.put("skd","application/x-koan");
+            _mimeMap.put("skm","application/x-koan");
+            _mimeMap.put("skp","application/x-koan");
+            _mimeMap.put("skt","application/x-koan");
+            _mimeMap.put("smi","application/smil");
+            _mimeMap.put("smil","application/smil");
+            _mimeMap.put("snd","audio/basic");
+            _mimeMap.put("spl","application/x-futuresplash");
+            _mimeMap.put("src","application/x-wais-source");
+            _mimeMap.put("sv4cpio","application/x-sv4cpio");
+            _mimeMap.put("sv4crc","application/x-sv4crc");
+            _mimeMap.put("swf","application/x-shockwave-flash");
+            _mimeMap.put("t","application/x-troff");
+            _mimeMap.put("tar","application/x-tar");
+            _mimeMap.put("tar.gz","application/x-gtar");
+            _mimeMap.put("tcl","application/x-tcl");
+            _mimeMap.put("tex","application/x-tex");
+            _mimeMap.put("texi","application/x-texinfo");
+            _mimeMap.put("texinfo","application/x-texinfo");
+            _mimeMap.put("tgz","application/x-gtar");
+            _mimeMap.put("tiff","image/tiff");
+            _mimeMap.put("tif","image/tiff");
+            _mimeMap.put("tr","application/x-troff");
+            _mimeMap.put("tsv","text/tab-separated-values");
+            _mimeMap.put("txt","text/plain");
+            _mimeMap.put("ustar","application/x-ustar");
+            _mimeMap.put("vcd","application/x-cdlink");
+            _mimeMap.put("vrml","model/vrml");
+            _mimeMap.put("wav","audio/x-wav");
+            _mimeMap.put("wbmp","image/vnd.wap.wbmp");
+            _mimeMap.put("wml","text/vnd.wap.wml");
+            _mimeMap.put("wmlc","application/vnd.wap.wmlc");
+            _mimeMap.put("wmls","text/vnd.wap.wmlscript");
+            _mimeMap.put("wmlsc","application/vnd.wap.wmlscriptc");
+            _mimeMap.put("wtls-ca-certificate","application/vnd.wap.wtls-ca-certificate");
+            _mimeMap.put("wrl","model/vrml");
+            _mimeMap.put("xbm","image/x-xbitmap");
+            _mimeMap.put("xml","text/xml");
+            _mimeMap.put("xpm","image/x-xpixmap");
+            _mimeMap.put("xwd","image/x-xwindowdump");
+            _mimeMap.put("xyz","chemical/x-pdb");
+            _mimeMap.put("zip","application/zip");
+            _mimeMap.put("z","application/compress");
+        }
         return _mimeMap;
     }
     
@@ -630,154 +772,22 @@ public class HandlerContext implements LifeCycle
      */
     public String getMimeByExtension(String filename)
     {
-        int i=filename.lastIndexOf(".");
-        String ext;
-
-        if (i<0 || i>=filename.length())
+        if (filename==null)
             return null;
         
-        ext=StringUtil.asciiToLowerCase(filename.substring(i+1));
-        
-        if (_mimeMap==null)
+        getMimeMap();
+        int i=-1;
+        while(true)
         {
-            _mimeMap = new HashMap();
-            _mimeMap.put("default","application/octet-stream");
-            _mimeMap.put("ai","application/postscript");
-            _mimeMap.put("aif","audio/x-aiff");
-            _mimeMap.put("aifc","audio/x-aiff");
-            _mimeMap.put("aiff","audio/x-aiff");
-            _mimeMap.put("asc","text/plain");
-            _mimeMap.put("au","audio/basic");
-            _mimeMap.put("avi","video/x-msvideo");
-            _mimeMap.put("bcpio","application/x-bcpio");
-            _mimeMap.put("bin","application/octet-stream");
-            _mimeMap.put("cdf","application/x-netcdf");
-            _mimeMap.put("class","application/octet-stream");
-            _mimeMap.put("cpio","application/x-cpio");
-            _mimeMap.put("cpt","application/mac-compactpro");
-            _mimeMap.put("csh","application/x-csh");
-            _mimeMap.put("css","text/css");
-            _mimeMap.put("dcr","application/x-director");
-            _mimeMap.put("dir","application/x-director");
-            _mimeMap.put("dms","application/octet-stream");
-            _mimeMap.put("doc","application/msword");
-            _mimeMap.put("dvi","application/x-dvi");
-            _mimeMap.put("dxr","application/x-director");
-            _mimeMap.put("eps","application/postscript");
-            _mimeMap.put("etx","text/x-setext");
-            _mimeMap.put("exe","application/octet-stream");
-            _mimeMap.put("ez","application/andrew-inset");
-            _mimeMap.put("gif","image/gif");
-            _mimeMap.put("gtar","application/x-gtar");
-            _mimeMap.put("hdf","application/x-hdf");
-            _mimeMap.put("hqx","application/mac-binhex40");
-            _mimeMap.put("html","text/html");
-            _mimeMap.put("htm","text/html");
-            _mimeMap.put("ice","x-conference/x-cooltalk");
-            _mimeMap.put("ief","image/ief");
-            _mimeMap.put("iges","model/iges");
-            _mimeMap.put("igs","model/iges");
-            _mimeMap.put("java","text/plain");
-            _mimeMap.put("jpeg","image/jpeg");
-            _mimeMap.put("jpe","image/jpeg");
-            _mimeMap.put("jpg","image/jpeg");
-            _mimeMap.put("js","application/x-javascript");
-            _mimeMap.put("jsp","text/plain");
-            _mimeMap.put("kar","audio/midi");
-            _mimeMap.put("latex","application/x-latex");
-            _mimeMap.put("lha","application/octet-stream");
-            _mimeMap.put("lzh","application/octet-stream");
-            _mimeMap.put("man","application/x-troff-man");
-            _mimeMap.put("me","application/x-troff-me");
-            _mimeMap.put("mesh","model/mesh");
-            _mimeMap.put("mid","audio/midi");
-            _mimeMap.put("midi","audio/midi");
-            _mimeMap.put("mif","application/vnd.mif");
-            _mimeMap.put("movie","video/x-sgi-movie");
-            _mimeMap.put("mov","video/quicktime");
-            _mimeMap.put("mp2","audio/mpeg");
-            _mimeMap.put("mp3","audio/mpeg");
-            _mimeMap.put("mpeg","video/mpeg");
-            _mimeMap.put("mpe","video/mpeg");
-            _mimeMap.put("mpga","audio/mpeg");
-            _mimeMap.put("mpg","video/mpeg");
-            _mimeMap.put("ms","application/x-troff-ms");
-            _mimeMap.put("msh","model/mesh");
-            _mimeMap.put("nc","application/x-netcdf");
-            _mimeMap.put("oda","application/oda");
-            _mimeMap.put("pbm","image/x-portable-bitmap");
-            _mimeMap.put("pdb","chemical/x-pdb");
-            _mimeMap.put("pdf","application/pdf");
-            _mimeMap.put("pgm","image/x-portable-graymap");
-            _mimeMap.put("pgn","application/x-chess-pgn");
-            _mimeMap.put("png","image/png");
-            _mimeMap.put("pnm","image/x-portable-anymap");
-            _mimeMap.put("ppm","image/x-portable-pixmap");
-            _mimeMap.put("ppt","application/vnd.ms-powerpoint");
-            _mimeMap.put("ps","application/postscript");
-            _mimeMap.put("qt","video/quicktime");
-            _mimeMap.put("ra","audio/x-pn-realaudio");
-            _mimeMap.put("ra","audio/x-realaudio");
-            _mimeMap.put("ram","audio/x-pn-realaudio");
-            _mimeMap.put("ras","image/x-cmu-raster");
-            _mimeMap.put("rgb","image/x-rgb");
-            _mimeMap.put("rm","audio/x-pn-realaudio");
-            _mimeMap.put("roff","application/x-troff");
-            _mimeMap.put("rpm","application/x-rpm");
-            _mimeMap.put("rpm","audio/x-pn-realaudio");
-            _mimeMap.put("rtf","application/rtf");
-            _mimeMap.put("rtf","text/rtf");
-            _mimeMap.put("rtx","text/richtext");
-            _mimeMap.put("sgml","text/sgml");
-            _mimeMap.put("sgm","text/sgml");
-            _mimeMap.put("sh","application/x-sh");
-            _mimeMap.put("shar","application/x-shar");
-            _mimeMap.put("silo","model/mesh");
-            _mimeMap.put("sit","application/x-stuffit");
-            _mimeMap.put("skd","application/x-koan");
-            _mimeMap.put("skm","application/x-koan");
-            _mimeMap.put("skp","application/x-koan");
-            _mimeMap.put("skt","application/x-koan");
-            _mimeMap.put("smi","application/smil");
-            _mimeMap.put("smil","application/smil");
-            _mimeMap.put("snd","audio/basic");
-            _mimeMap.put("spl","application/x-futuresplash");
-            _mimeMap.put("src","application/x-wais-source");
-            _mimeMap.put("sv4cpio","application/x-sv4cpio");
-            _mimeMap.put("sv4crc","application/x-sv4crc");
-            _mimeMap.put("swf","application/x-shockwave-flash");
-            _mimeMap.put("t","application/x-troff");
-            _mimeMap.put("tar","application/x-tar");
-            _mimeMap.put("tcl","application/x-tcl");
-            _mimeMap.put("tex","application/x-tex");
-            _mimeMap.put("texi","application/x-texinfo");
-            _mimeMap.put("texinfo","application/x-texinfo");
-            _mimeMap.put("tiff","image/tiff");
-            _mimeMap.put("tif","image/tiff");
-            _mimeMap.put("tr","application/x-troff");
-            _mimeMap.put("tsv","text/tab-separated-values");
-            _mimeMap.put("txt","text/plain");
-            _mimeMap.put("ustar","application/x-ustar");
-            _mimeMap.put("vcd","application/x-cdlink");
-            _mimeMap.put("vrml","model/vrml");
-            _mimeMap.put("wav","audio/x-wav");
-            _mimeMap.put("wbmp","image/vnd.wap.wbmp");
-            _mimeMap.put("wml","text/vnd.wap.wml");
-            _mimeMap.put("wmlc","application/vnd.wap.wmlc");
-            _mimeMap.put("wmls","text/vnd.wap.wmlscript");
-            _mimeMap.put("wmlsc","application/vnd.wap.wmlscriptc");
-            _mimeMap.put("wtls-ca-certificate","application/vnd.wap.wtls-ca-certificate");
-            _mimeMap.put("wrl","model/vrml");
-            _mimeMap.put("xbm","image/x-xbitmap");
-            _mimeMap.put("xml","text/xml");
-            _mimeMap.put("xpm","image/x-xpixmap");
-            _mimeMap.put("xwd","image/x-xwindowdump");
-            _mimeMap.put("xyz","chemical/x-pdb");
-            _mimeMap.put("zip","application/zip");
-        }
+            i=filename.indexOf(".",i+1);
+
+            if (i<0 || i>=filename.length())
+                return null;
         
-        String type = (String)_mimeMap.get(ext);
-        return type;
+            String ext=StringUtil.asciiToLowerCase(filename.substring(i+1));
+            String type = (String)_mimeMap.get(ext);
+            return type;
+        }
     }
 
     /* ------------------------------------------------------------ */
@@ -787,8 +797,7 @@ public class HandlerContext implements LifeCycle
      */
     public void setMimeMapping(String extension,String type)
     {
-        getMimeByExtension("default");
-        _mimeMap.put(extension,type);
+        getMimeMap().put(extension,type);
     }
 
     /* ------------------------------------------------------------ */
