66800acc96b119e1d18026bf12d3444f06453273
raif <>
2001-03-16 23:27:01 +0000

    + modified to include needed jars and system-wide preferences needed for SASL.

--- Jetty/bin/jetty.bat
+++ Jetty/bin/jetty.bat
@@ -5,15 +5,15 @@
 :: Configuration files
 ::
 :: %HOME%\.jettyrc
-::   If found, it is called at the start of batch file. It may perform 
+::   If found, it is called at the start of batch file. It may perform
 ::   any sequence of commands, like setting relevant environment variables.
 ::
 :: %JETTY_HOME%\etc\jetty.conf (NOTE: !!! THIS IS NOT WORKING AT THE MOMENT !!!)
 ::   If found, and no configurations were given on the command line,
-::   the file will be used as this script's configuration. 
+::   the file will be used as this script's configuration.
 ::   Each line in the file may contain:
 ::     - A comment denoted by the pound (#) sign as first non-blank character.
-::     - The path to a regular file, which will be passed to jetty as a 
+::     - The path to a regular file, which will be passed to jetty as a
 ::       config.xml file.
 ::     - The path to a directory. Each *.xml file in the directory will be
 ::       passed to jetty as a config.xml file.
@@ -22,15 +22,15 @@
 ::
 :: Configuration variables
 ::
-:: JAVA_HOME  
-::   Home of Java installation. This needs to be set as this script 
+:: JAVA_HOME
+::   Home of Java installation. This needs to be set as this script
 ::   will not look for it.
 ::
 :: JAVA
 ::   Command to invoke Java. If not set, %JAVA_HOME%\bin\java will be
 ::   used.
 ::
-:: JETTY_HOME 
+:: JETTY_HOME
 ::   Where Jetty is installed. If not set, the script will try go
 ::   guess it by first looking at the invocation path for the script,
 ::   and then by looking in standard locations as %HOME%\opt\jetty
@@ -39,8 +39,8 @@
 ::
 ::    <Arg><SystemProperty name="jetty.home" default="."\>\webapps\jetty.war<\Arg>
 ::
-:: JETTY_LOG 
-::   Where jetty logs should be stored. The only effect of this 
+:: JETTY_LOG
+::   Where jetty logs should be stored. The only effect of this
 ::   variable is to set the "jetty.log" java system property so
 ::   configure.xml files can use it, f.e.:
 ::
@@ -51,7 +51,7 @@
 ::
 :: JETTY_RUN
 ::   Where temporary files should be stored. It defaults to %TMP%.
-::   
+::
 :: $Id$
 ::
 
@@ -103,14 +103,14 @@ if not x==x%JETTY_HOME% goto check_home_jetty
 :: No JETTY_HOME yet? We're out of luck!
 :::::::::::::::::::::::::::::::::::::::::::::::::::
 if not x==x%JETTY_HOME% goto check_jar_jetty
-    echo "** ERROR: JETTY_HOME not set, you need to set it or install in a standard location" 
+    echo "** ERROR: JETTY_HOME not set, you need to set it or install in a standard location"
     goto ERROR
 
 :check_jar_jetty
 if exist %JETTY_HOME%\%JETTY_JAR% goto have_home_jetty
-   echo "** ERROR: Oops! %JETTY_HOME%\%JETTY_JAR% is not readable!" 
+   echo "** ERROR: Oops! %JETTY_HOME%\%JETTY_JAR% is not readable!"
    goto ERROR
-   
+
 :have_home_jetty
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Get the list of config.xml files from the command line.
@@ -143,7 +143,7 @@ if not x==x%CONFIGS% goto have_configs
 if not x==x%CONFIG_LINES (
   for /f %%C in (%CONFIG_LINES%) do (
     if not exist "%%C" (
-      echo "** WARNING: Cannot read '%%C' specified in '%JETTY_CONF%' 
+      echo "** WARNING: Cannot read '%%C' specified in '%JETTY_CONF%'
     ) else (
     if not exist "%%C\*.xml" (
       :: assume it's a configure.xml file
@@ -177,8 +177,8 @@ if not x==x%CONFIGS% goto have_configs
 echo CONFIGS=%CONFIGS%
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: Check if the admin servlet was added. 
-:: Add it if not. 
+:: Check if the admin servlet was added.
+:: Add it if not.
 :: It will be needed to stop jetty cleanly.
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::@TODO Find a way to do this !!!
@@ -220,6 +220,8 @@ if x==x%JAVA% (
 set CP=%JETTY_HOME%\lib\javax.servlet.jar
 set CP=%CP%;%JETTY_HOME%\lib\com.mortbay.jetty.jar
 set CP=%CP%;%JETTY_HOME%\lib\com.microstar.xml.jar
+set CP=%CP%;%JETTY_HOME%\lib\cryptix-sasl-jetty.jar;%JETTY_HOME%\lib\javax-sasl.jar
+set CP=%CP%;%JETTY_HOME%\lib\log4j.jar
 if exist "%JETTY_HOME%\LIB\org.apache.jasper.jar"  set CP=%CP%;%JETTY_HOME%\lib\org.apache.jasper.jar
 if exist "%JETTY_HOME%\LIB\com.sun.net.ssl.jar"    set CP=%CP%;%JETTY_HOME%\lib\com.sun.net.ssl.jar
 if exist "%JAVA_HOME%\lib\tools.jar"               set CP=%CP%;%JAVA_HOME%\lib\tools.jar
@@ -228,7 +230,8 @@ set CLASSPATH=%CP%;%CLASSPATH%
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Add jetty properties to Java VM options.
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::
-set JAVA_OPTIONS=-Djetty.home="%JETTY_HOME%" -Djetty.log="%JETTY_LOG%" %JAVA_OPTIONS%
+rem set JAVA_OPTIONS=-Djetty.home="%JETTY_HOME%" -Djetty.log="%JETTY_LOG%" %JAVA_OPTIONS%
+set JAVA_OPTIONS=-Djetty.home="%JETTY_HOME%" -Djetty.log="%JETTY_LOG%" -Djavax.security.sasl.server.pkgs=cryptix.sasl -Dcryptix.sasl.srp.passwordfile=etc/tpasswd %JAVA_OPTIONS%
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: This is how the Jetty server will be started
@@ -237,7 +240,7 @@ set RUN_CMD=%JAVA% %JAVA_OPTIONS% com.mortbay.Jetty.Server %CONFIGS%
 
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: Comment these out after you're happy with what 
+:: Comment these out after you're happy with what
 :: the script is doing.
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::
 echo JETTY_HOME     =  %JETTY_HOME%
@@ -262,7 +265,7 @@ if not "%ACTION%"=="start" goto try_stop
         echo "STARTED `date`" >>%JETTY_LOG%\jetty.out
         start %RUN_CMD% >> %JETTY_LOG%\jetty.out
         goto END
-        
+
 :try_stop
 if not "%ACTION%"=="stop" goto try_restart
         echo "Shutting down Jetty: "
@@ -271,25 +274,25 @@ if not "%ACTION%"=="stop" goto try_restart
         del /q /f %JETTY_RUN%\jetty.pid
         echo "STOPPED `date`" >> %JETTY_LOG%\jetty.out
         goto END
-        
+
 :try_restart
 if not "%ACTION%"=="restart" goto try_run
         %0 stop %*
         sleep 5
         %0 start %*
         goto END
-        
+
 :try_run
 if not "%ACTION%"=="run" goto try_check
         echo "Running Jetty: "
         echo "STARTED `date`" >>%JETTY_LOG%\jetty.out
         %RUN_CMD%
         goto END
-        
+
 :try_check
 if not "%ACTION%"=="check" goto no_action
         echo "Checking arguments to Jetty: "
-        :: do nothing 
+        :: do nothing
         goto END
 
 :no_action
