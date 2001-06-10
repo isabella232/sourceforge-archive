e189b857c8c61486208335cc4a33e6811e636861
bretts <>
2001-06-10 13:40:02 +0000

    Small typo in the javadoc.

--- Jetty/src/com/mortbay/Jetty/Servlet/ServletHolder.java
+++ Jetty/src/com/mortbay/Jetty/Servlet/ServletHolder.java
@@ -122,7 +122,7 @@ public class ServletHolder
 
     /* ------------------------------------------------------------ */
     /**
-     * @deprecated Use getInitORder()
+     * @deprecated Use getInitOrder()
      */
     public boolean isInitOnStartup()
     {
