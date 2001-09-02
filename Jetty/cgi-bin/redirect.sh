4ae54cd0ddeb471fdfb791ee227b0c763330416b
username_to_access_repo <>
2001-09-02 23:38:39 +0000

    This commit was manufactured by cvs2svn to create tag 'Jetty3Extra-1_0_0'.

--- Jetty/cgi-bin/redirect.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Test redirecting from CGI
-# $Id$
-echo "Status: 302 Moved"
-echo "Location: http://${HTTP_HOST}/"
-echo
--- Jetty/win32/jettyenv.bat
+++
@@ -1,1 +0,0 @@
-SET CLASSPATH=%JETTY_HOME%\lib\com.mortbay.jetty.jar;%JETTY_HOME%\lib\com.sun.net.ssl.jar;%JETTY_HOME%\lib\javax.servlet.jar;%JETTY_HOME%\lib\org.apache.jasper.jar;%JETTY_HOME%\lib\com.microstar.xml.jar
