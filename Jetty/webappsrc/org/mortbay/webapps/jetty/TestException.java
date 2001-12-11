2086d55175b6e70649c9e7165c8becfb19ba1886
gregwilkins <>
2001-12-11 05:24:19 +0000

    test error redirect

---
+++ Jetty/webappsrc/org/mortbay/webapps/jetty/TestException.java
@@ -0,0 +1,15 @@
+
+package org.mortbay.webapps.jetty;
+
+public class TestException extends Exception
+{
+    public TestException()
+    {
+        super();
+    }
+    
+    public TestException(String m)
+    {
+        super(m);
+    }
+}
