6bca78916f6b012ac3f39d4184442572228b9981
gregwilkins <>
2002-02-07 01:43:32 +0000

    demo filter

---
+++ Jetty/demo/src/org/mortbay/webapps/jetty/DemoFilter.java
@@ -0,0 +1,43 @@
+// ===========================================================================
+// Copyright (c) 1996 Mort Bay Consulting Pty. Ltd. All rights reserved.
+// $Id$
+// ---------------------------------------------------------------------------
+
+package org.mortbay.webapps.jetty;
+import org.mortbay.util.Code;
+import org.mortbay.util.Log;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public  class DemoFilter implements Filter
+{
+    public void init(FilterConfig filterConfig)
+        throws ServletException
+    {
+        Code.debug("init:",filterConfig);
+    }
+    
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain)
+	throws IOException, ServletException
+    {
+        System.err.println(request);
+        Code.debug("doFilter:",request);
+        chain.doFilter(request, response);
+    }
+
+    public void destroy()
+    {
+        Code.debug("destroy");
+    }
+}
+
