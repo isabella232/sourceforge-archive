85585f8ce68a03164174c6724120ae196a868165
username_to_access_repo <>
2002-04-18 17:52:16 +0000

    This commit was manufactured by cvs2svn to create tag 'Jetty_4_0_1RC0'.

--- JettyOld/contrib/com/mortbay/Jetty/com_mortbay_Jetty_SetUID.c
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-#include "com_mortbay_Jetty_SetUID.h"
-#include <unistd.h>
-
-JNIEXPORT void JNICALL Java_com_mortbay_Jetty_SetUID_doSetUID
-    (JNIEnv * env, jclass class, jint uid)
-{
-    setuid(uid);
-}
