9d20cfe1cebd57396533ddbc651096ba070c3f97
gregwilkins <>
2002-07-19 08:09:28 +0000

    removed dynamic attribute

--- Jetty/src/org/mortbay/jetty/servlet/jmx/ServletHttpContextMBean.java
+++ Jetty/src/org/mortbay/jetty/servlet/jmx/ServletHttpContextMBean.java
@@ -52,7 +52,6 @@ public class ServletHttpContextMBean extends HttpContextMBean
     {
         super.defineManagedResource();
 
-        defineAttribute("dynamicServletPathSpec");
         defineOperation("addServlet",
                         new String[] {STRING,STRING,STRING},
                         IMPACT_ACTION);
