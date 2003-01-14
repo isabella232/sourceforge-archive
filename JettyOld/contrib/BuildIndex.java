a019b225c4804378c1af4b11fdd6c389a2cdb0ea
username_to_access_repo <>
2003-01-14 10:18:29 +0000

    This commit was manufactured by cvs2svn to create tag 'Jetty_4_2_5'.

--- JettyOld/contrib/BuildIndex.java
+++
@@ -1,7 +0,0 @@
-class BuildIndex { Class[] classes = {
-    com.mortbay.Servlets.PackageIndex.Package.class,
-    com.mortbay.Servlets.PackageIndex.PackageIndex.class,
-    com.mortbay.Servlets.PackageIndex.PackageSpec.class,
-    com.mortbay.Servlets.PackageIndex.PackageVersionOrderer.class,
-    com.mortbay.Servlets.PackageIndex.PackageVersionOrdererTest.class,
-};}
