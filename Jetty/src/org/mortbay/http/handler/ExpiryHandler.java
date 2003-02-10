136cec650b339e91a5d93dd853ba3e9f58a6e8a7
bretts <>
2003-02-10 00:33:54 +0000

    Reformat to coding standard. Updated javadoc.

--- Jetty/src/org/mortbay/http/handler/ExpiryHandler.java
+++ Jetty/src/org/mortbay/http/handler/ExpiryHandler.java
@@ -16,7 +16,7 @@ import org.mortbay.util.Code;
 /* ------------------------------------------------------------ */
 /**
  * Handler that allows the default Expiry of all content to be set.
- * 
+ *
  * @version $Id$
  * @author Brett Sealey
  */
@@ -27,31 +27,44 @@ public class ExpiryHandler extends AbstractHttpHandler
      */
     private long _ttl=-1;
 
+    /* ------------------------------------------------------------ */
     /**
      * Set the default expiry time in seconds.
      *
      * @param ttl The default time to live in seconds. If negative (the
      * default) then all content will be set to expire 01Jan1970 by default.
      */
-    public void setTimeToLive(long ttl) {
-	_ttl=ttl;
+    public void setTimeToLive(long ttl)
+    {
+        _ttl=ttl;
     }
-    
+
     /* ------------------------------------------------------------ */
+    /** Handle a request by pre-populating the Expires header with a a value
+     * that corresponds to now + ttl. If ttl -s negative then
+     * HttpFields.__01Jan1970 is used.
+     *
+     * Settings made here can be overridden by subsequent handling of the
+     * request.
+     *
+     * @param pathInContext The context path
+     * @param pathParams Path parameters such as encoded Session ID
+     * @param request The HttpRequest request
+     * @param response The HttpResponse response
+     */
     public void handle(String pathInContext,
                        String pathParams,
                        HttpRequest request,
                        HttpResponse response)
-        throws HttpException, IOException
+            throws HttpException,IOException
     {
         Code.debug("ExpiryHandler.handle()");
-	String expires;
-	if (_ttl<0)
-	    expires=HttpFields.__01Jan1970;
-	else
-	    expires=HttpFields.__dateSend
-		.format(new
-		    Date(System.currentTimeMillis()+1000L*_ttl));
-	response.setField(HttpFields.__Expires,expires);
+        String expires;
+        if (_ttl<0)
+            expires=HttpFields.__01Jan1970;
+        else
+            expires=HttpFields.__dateSend
+                    .format(new Date(System.currentTimeMillis()+1000L*_ttl));
+        response.setField(HttpFields.__Expires,expires);
     }
 }
