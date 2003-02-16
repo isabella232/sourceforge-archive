85b7a0f35b3e981deb7fc5c9dbba120f01c6628a
gregwilkins <>
2003-02-16 12:43:36 +0000

    Added paths to the ServletHolderMBean

--- Jetty/src/org/mortbay/jetty/servlet/jmx/ServletHolderMBean.java
+++ Jetty/src/org/mortbay/jetty/servlet/jmx/ServletHolderMBean.java
@@ -9,7 +9,9 @@ import javax.management.MBeanException;
 import javax.management.MBeanServer;
 import org.mortbay.util.jmx.LifeCycleMBean;
 import org.mortbay.jetty.servlet.ServletHolder;
+import org.mortbay.jetty.servlet.ServletHandler;
 import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.Map;
 import javax.management.ObjectName;
 import org.mortbay.util.Code;
@@ -39,7 +41,25 @@ public class ServletHolderMBean extends HolderMBean
     {
         super.defineManagedResource();
         defineAttribute("initOrder");
+        defineAttribute("paths",READ_ONLY,ON_MBEAN);
         _holder=(ServletHolder)getManagedResource();
     }
+
+    /* ------------------------------------------------------------ */
+    public String[] getPaths()
+    {
+        ServletHandler handler = (ServletHandler)_holder.getHttpHandler();
+        Map servletMap = handler.getServletMap();
+        ArrayList paths = new ArrayList(servletMap.size());
+        Iterator iter = servletMap.entrySet().iterator();
+        while (iter.hasNext())
+        {
+            Map.Entry entry =(Map.Entry)iter.next();
+            if (entry.getValue()==_holder)
+                paths.add(entry.getKey());
+        }
+        return (String[])paths.toArray(new String[paths.size()]);
+    }
+    
     
 }
