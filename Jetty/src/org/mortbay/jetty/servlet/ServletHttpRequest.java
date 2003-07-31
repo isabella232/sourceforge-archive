becba06feea02749e5da00cb1569328f2ac7bc7c
gregwilkins <>
2003-07-31 07:42:45 +0000

    private char encoding for servlet requests

--- Jetty/src/org/mortbay/jetty/servlet/ServletHttpRequest.java
+++ Jetty/src/org/mortbay/jetty/servlet/ServletHttpRequest.java
@@ -80,6 +80,7 @@ public class ServletHttpRequest
     private int _inputState=0;
     private ServletHolder _servletHolder;
     private String _pathInContext;
+    private String _characterEncoding;
     
     /* ------------------------------------------------------------ */
     /** Constructor. 
@@ -113,6 +114,7 @@ public class ServletHttpRequest
         _reader=null;
         _inputState=0;
         _servletHolder=null;
+        _characterEncoding=null;
 
         if (servletHandler!=null)
             _contextPath=_servletHandler.getHttpContext().getContextPath();
@@ -611,12 +613,14 @@ public class ServletHttpRequest
         if (_inputState!=0)
             throw new IllegalStateException("getReader() or getInputStream() called");
         "".getBytes(encoding);
-        _httpRequest.setCharacterEncoding(encoding);
+        _characterEncoding=encoding;
     }
     
     /* -------------------------------------------------------------- */
     public String getCharacterEncoding()
     {
+        if (_characterEncoding!=null)
+            return _characterEncoding;
         return _httpRequest.getCharacterEncoding();
     }
     
@@ -752,7 +756,7 @@ public class ServletHttpRequest
         {
             try
             {
-                String encoding=getCharacterEncoding();
+                String encoding=_characterEncoding!=null?_characterEncoding:getCharacterEncoding();
                 if (encoding==null)
                     encoding=StringUtil.__ISO_8859_1;
                 _reader=new BufferedReader(new InputStreamReader(getInputStream(),encoding));
