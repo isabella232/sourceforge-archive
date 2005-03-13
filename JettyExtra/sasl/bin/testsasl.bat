959d2f605e6ae97c3b331b6f612ea9db2aeff190
username_to_access_repo <>
2005-03-13 20:59:35 +0000

    This commit was manufactured by cvs2svn to create tag 'Jetty_5_1_3rc1'.

--- JettyExtra/sasl/bin/testsasl.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set CP=%JETTY_HOME%\lib\cryptix-sasl-jetty.jar;%JETTY_HOME%\lib\javax-sasl.jar
-set CP=%CP%;%JETTY_HOME%\lib\com.mortbay.jetty.jar
-
-set CLASSPATH=%CP%;%CLASSPATH%
-
-%JAVA_HOME%\bin\java -Djavax.security.sasl.client.pkgs=cryptix.sasl com.mortbay.HTTP.SASL.SaslTest %1 %2 %3
