b7a9ea7e00b27ff4c8834439ce654d9ed43d581e
gregwilkins <>
2005-07-29 09:14:59 +0000

    includes

--- Jetty/src/org/mortbay/http/HttpInputStream.java
+++ Jetty/src/org/mortbay/http/HttpInputStream.java
@@ -23,7 +23,6 @@ import java.io.UnsupportedEncodingException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.jboss.mq.selectors.ISelectorParser;
 import org.mortbay.util.LineInput;
 import org.mortbay.util.StringUtil;
 
