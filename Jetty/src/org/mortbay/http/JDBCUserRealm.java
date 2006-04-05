c3b4ba41fca8c45297c003c15460134107bc3a00
janb <>
2006-04-05 12:59:16 +0000

    Stop JDBCUserRealm forcing all credentials to type String

--- Jetty/src/org/mortbay/http/JDBCUserRealm.java
+++ Jetty/src/org/mortbay/http/JDBCUserRealm.java
@@ -185,33 +185,27 @@ public class JDBCUserRealm extends HashUserRealm
                                   Object credentials,
                                   HttpRequest request)
     {
-        if (credentials==null)
-            return authenticate(username,null,request);
-        if (credentials instanceof String)
-            return authenticate(username,(String)credentials,request);
-        return authenticate(username,credentials.toString(),request);
-    }
-    
-    /* ------------------------------------------------------------ */
-    public synchronized Principal authenticate(String username,
-                                               String credentials,
-                                               HttpRequest request)
-    {
-        long now = System.currentTimeMillis();
-        if (now - _lastHashPurge > _cacheTime || _cacheTime == 0)
-        {
-            super.clear();
-            _roles.clear();
-            _lastHashPurge = now;
-        }
-        Principal user = (Principal)super.get(username);
-        if (user == null)
-        {
-            loadUser(username);
-            user = (Principal)super.get(username);
-        }
+
+    	synchronized(this)
+    	{
+    		long now = System.currentTimeMillis();
+    		if (now - _lastHashPurge > _cacheTime || _cacheTime == 0)
+    		{
+    			super.clear();
+    			_roles.clear();
+    			_lastHashPurge = now;
+    		}
+    		Principal user = (Principal)super.get(username);
+    		if (user == null)
+    		{
+    			loadUser(username);
+    			user = (Principal)super.get(username);
+    		}
+    	}
         return super.authenticate(username, credentials, request);
     }
+    
+   
     
     /* ------------------------------------------------------------ */
     private void loadUser(String username)
